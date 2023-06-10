d144950708d7580c671d5d342c6980850e517ba9
abdealipython <abdealipython@gmail.com>
2023-06-10 10:36:40 -0400

    Test commit

---
+++ uat_app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+myhome="HOME PAGE!!!!!!!!!!!!!!!!!!!!!!!"
+
+
+@app.route("/")		#http://127.0.0.1:5000
+def get_home():
+	return myhome
+
+@app.route("/trainer")		#http://127.0.0.1:5000/contact
+def trainer():
+	return render_template("trainer_details.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=True,host="0.0.0.0")
+
+
+
